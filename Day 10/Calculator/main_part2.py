24b356a092bbc4e3b6678cd161730bec7be97206
williamlaster <105891104+williamlaster@users.noreply.github.com>
2022-05-31 18:52:37 -0500

    Add files via upload

---
+++ Day 10/Calculator/main_part2.py
@@ -0,0 +1,56 @@
+# Calculator
+import art
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+print(art.logo)
+
+
+def calculator():
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        run = input(
+            f"Type 'y' to continue calculating with {answer}, type 'r' to restart, type 'n' to quit: ").lower()
+        if run == "y":
+            num1 = answer
+        elif run == "r":
+            should_continue = False
+            calculator()
+        else:
+            should_continue = False
+
+
+calculator()
