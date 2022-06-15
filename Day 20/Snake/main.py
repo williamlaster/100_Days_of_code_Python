f6642a8d98b9133186f2697d180aadab8281cc1f
williamlaster <105891104+williamlaster@users.noreply.github.com>
2022-06-14 20:03:53 -0500

    initial upload for day 20

---
+++ Day 20/Snake/main.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+# Set controls for the snake
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.09)
+    snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    # Logic to make the game end if the snake head hits the edge
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        scoreboard.game_over()
+        game_is_on = False
+    # Logic to make the game end if the snake head hits any part of the body
+    for segment in snake.segments[1:]:
+        if snake.head.pos() == segment.pos():
+            scoreboard.game_over()
+            game_is_on = False
+
+
+screen.exitonclick()
---
+++ Day 20/Snake/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Arial', 18, 'normal')
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.speed(0)
+        self.penup
+        self.goto(0, 270)
+        self.pencolor("white")
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", move=False,
+                   align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
